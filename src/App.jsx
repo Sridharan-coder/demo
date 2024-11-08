c45e41b4b67b2d6f9135f7836f1e89fd9156777f
Sridharan-coder <sridharan.r@mitrahsoft.com>
2024-11-08 20:39:13 +0530

    Game Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
-import './App.css';
 import { useEffect, useState } from 'react';
 
+import './App.css';
+
+import { Button, Grid2, Link, Menu, MenuItem, Stack, Typography } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import { LuUndo2 } from "react-icons/lu";
+import { IoRemoveOutline } from "react-icons/io5";
+import { RiSwap2Fill, RiVipCrownFill } from "react-icons/ri";
+import { BsGridFill } from "react-icons/bs";
 
 
 function App() {
@@ -14,10 +21,20 @@ function App() {
   const [score, SetScore] = useState(0)
   const [moves, setMoves] = useState(0)
   const [highScore, setHighScore] = useState(0)
+  const [anchorEl, setAnchorEl] = useState(null);
 
   const positiongrid = [14, 132, 250, 368]
 
   let numberGrid = grid
+
+  const open = Boolean(anchorEl);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
   const defaultlayout = (i) => {
     const temp = []
@@ -78,7 +95,7 @@ function App() {
         <g key={`dynamicposition=${startColumn}-${startRow}`}>
           <>
             <rect x={startRow} y={startColumn} width="108" height="108" rx="10" fill={numberGrid[i][j] ? boxColor[numberGrid[i][j]] : "#BAAC9A"} />
-            <text x={numberXAlign} y={startColumn + y} style={{ fontSize: 39, fontWeight: 500 }} ><tspan fill='white'>{numberGrid[i][j] || ""}</tspan></text>
+            <text x={numberXAlign} y={startColumn + y} style={{ fontSize: 39, fontWeight: 500 }} ><tspan fill='rgb(135 114 89)' style={{ fontWeight: 'bold' }}>{numberGrid[i][j] || ""}</tspan></text>
           </>
         </g>
       )
@@ -86,7 +103,6 @@ function App() {
     }
     return temp;
   }
-
 
   const leftArrow = () => {
     setMoves(moves + 1)
@@ -102,7 +118,7 @@ function App() {
           filteredArray[j + 1] = 0;
           tempscore += filteredArray[j]
           for (let k = j + 1; k < filteredArray.length - 1; k++) {
-            if (filteredArray[k] == 0) {
+            if (filteredArray[k] === 0) {
               filteredArray[k] = filteredArray[k + 1];
               filteredArray[k + 1] = 0
             }
@@ -141,7 +157,7 @@ function App() {
           filteredArray[j - 1] = 0;
           tempscore += filteredArray[j]
           for (let k = j - 1; k > 0; k--) {
-            if (filteredArray[k] == 0) {
+            if (filteredArray[k] === 0) {
               filteredArray[k] = filteredArray[k - 1];
               filteredArray[k - 1] = 0
             }
@@ -187,7 +203,7 @@ function App() {
           filteredArray[j + 1] = 0;
           tempscore += filteredArray[j]
           for (let k = j + 1; k < filteredArray.length - 1; k++) {
-            if (filteredArray[k] == 0) {
+            if (filteredArray[k] === 0) {
               filteredArray[k] = filteredArray[k + 1];
               filteredArray[k + 1] = 0
             }
@@ -212,7 +228,6 @@ function App() {
     setGrid(numberGrid)
   }
 
-
   const upArrow = () => {
 
     setMoves(moves + 1)
@@ -235,7 +250,7 @@ function App() {
           filteredArray[j + 1] = 0;
           tempscore += filteredArray[j]
           for (let k = j + 1; k < filteredArray.length - 1; k++) {
-            if (filteredArray[k] == 0) {
+            if (filteredArray[k] === 0) {
               filteredArray[k] = filteredArray[k + 1];
               filteredArray[k + 1] = 0
             }
@@ -297,7 +312,7 @@ function App() {
     }
     let random;
     let randomArray;
-    console.log(positionChoosing);
+    // console.log(positionChoosing);
 
     if (positionChoosing.length) {
       random = positionChoosing[createDataRandom(positionChoosing)];
@@ -309,22 +324,28 @@ function App() {
     else {
       alert("new Game")
       newGame()
-      return
+
     }
 
   }
 
 
   const newGame = () => {
+    console.log("ko");
+
     const intialArray = [
       [0, 0, 0, 0],
       [0, 0, 0, 0],
       [0, 0, 0, 0],
       [0, 0, 0, 0],
     ]
+    console.log(intialArray);
+
+    // setTimeout(() => {
     numberGrid = intialArray;
     setMoves(0)
-    setHighScore(highScore < score ? score : highScore)
+    console.log(highScore);
+    setHighScore(highScore <= score ? score : highScore);
     SetScore(0)
     let one = createData();
     let two = createData();
@@ -339,27 +360,136 @@ function App() {
     intialArray[one[0]][one[1]] = 2
     intialArray[two[0]][two[1]] = 2
     setGrid([...intialArray]);
-    
+    console.log(intialArray);
+    // }, 3000)
+
+
   }
 
   useEffect(() => {
     newGame();
+    // eslint-disable-next-line
   }, [])
-
 
   return (
     <>
-      <p>Score : {score}</p>
-      <p>Moves : {moves}</p>
-      <p>High Score : {highScore}</p>
-      <button onClick={() => newGame()}>New Game</button>
-      <svg width={576} height={576} viewBox='-100 -100 676 676' xmlns="http://www.w3.org/2000/svg" >
-        <g>
-          <rect width="492" height="492" rx="10" fill='#9c8979' />
-        </g>
-        {positiongrid.map((i) => defaultlayout(i))}
-        {positiongrid.map((numX) => updatedLayout(numX, positiongrid.indexOf(numX)))}
-      </svg>
+      <Grid2 container alignContent={'center'} marginTop={3}>
+        <Grid2 containe size={5}>
+          <Button
+            id="basic-button"
+            aria-controls={open ? 'basic-menu' : undefined}
+            aria-haspopup="true"
+            aria-expanded={open ? 'true' : undefined}
+            onClick={handleClick}
+            style={{ display: 'flex', alignItems: 'center', height: 43, color: 'rgb(135 114 89)', fontFamily: 'inherit' }}
+          >
+            <MenuIcon /> <Typography gutterBottom marginTop={2} fontWeight={700} fontSize={"3rem"}>
+              2048
+            </Typography>
+          </Button>
+          <Menu
+            id="basic-menu"
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}
+            MenuListProps={{
+              'aria-labelledby': 'basic-button',
+            }}
+            defaultChecked={1}
+          >
+            <MenuItem onClick={handleClose} key={1}>Standard</MenuItem>
+            <MenuItem onClick={handleClose}>Classic</MenuItem>
+            <MenuItem onClick={handleClose}>Tutorial</MenuItem>
+          </Menu>
+        </Grid2>
+        <Grid2 container justifyContent={'flex-start'} size={2} >
+          <Grid2 size={2.8}></Grid2>
+          <Grid2 container alignContent={'center'} direction={'column'} size={4} className="scoreBoard" borderRadius={3}>
+            <Grid2 textAlign={'center'} className="boardName">
+              SCORE
+            </Grid2>
+            <Grid2 textAlign={'center'} className="boardValues">
+              {score}
+            </Grid2>
+          </Grid2>
+          <Grid2 size={0.5}></Grid2>
+          <Grid2 container alignContent={'center'} direction={'column'} size={4} className="bestScoreBoard" borderRadius={3}>
+            <Grid2 textAlign={'center'} className="boardName">
+              BEST
+            </Grid2>
+            <Grid2 textAlign={'center'} className="boardValues">
+              {highScore}
+            </Grid2>
+          </Grid2>
+          <Grid2 size={0.7}></Grid2>
+        </Grid2>
+        <Grid2 container alignItems={'center'} size={5}>
+          <Grid2 size={6.4}></Grid2>
+          <Grid2 size={2.3}>
+            <Link href="https://docs.google.com/forms/d/e/1FAIpQLScZldyVgauCIUTfGVlV90-7Za_6kYUSMGcaNmh6WGIfOAK_RA/
+            viewform?usp=pp_url&entry.1880902338=Using%20worker%3A%20true%0ARenderer%20initialized%20once%3A%20true%
+            0AWorker%20startup%20failed%3A%20false%0AResolution%3A%203%0AWeb%20worker%20supported%3A%20true%0AUser%
+            20agent%3A%20Mozilla%2F5.0%20(Windows%20NT%2010.0%3B%20Win64%3B%20x64)%20AppleWebKit%2F537.36%20(KHTML%2C%20like%20Gecko)
+            %20Chrome%2F130.0.0.0%20Safari%2F537.36%20Edg%2F130.0.0.0%0AWindow%20width%3A%201592%0AWindow%20height%3A%20921%0AScreen%
+            20width%3A%201600%0AScreen%20height%3A%201000%0ADevice%20pixel%20ratio%3A%202%0ACurrent%20URL%3A%20https%3A%2F%2Fplay2048
+            .co%2F%0ACookies%20enabled%3A%20true%0APlatform%3A%20Win32%0ABrowser%20version%3A%205.0%20(Windows%20NT%2010.
+            0%3B%20Win64%3B%20x64)%20AppleWebKit%2F537.36%20(KHTML%2C%20like%20Gecko)%20Chrome%2F130.0.0.0%20Safari%2F537.
+            36%20Edg%2F130.0.0.0%0ABrowser%20vendor%3A%20Google%20Inc.%0ABrowser%20product%3A%20Gecko%0ADevice%20memory%3A%208"
+              underline='hover'
+              color='inherit'
+              fontSize={16}
+            >
+              Give Feedback
+            </Link>
+          </Grid2>
+          <Grid2 size={3}>
+            <Stack spacing={2.1} >
+              <Button variant="contained" className='newGameButton' onClick={() => newGame()}>New Game</Button>
+            </Stack>
+          </Grid2>
+          <Grid2 size={0.2}></Grid2>
+
+
+        </Grid2>
+      </Grid2>
+
+      <Grid2 container justifyContent={'center'} size={12} marginTop={4}>
+        <Grid2 size={5.4} justifyContent={'center'}>
+          <svg width={576} height={530} viewBox='0 0 376 610' xmlns="http://www.w3.org/2000/svg" >
+            <g>
+              <rect width="492" height="492" rx="25" fill='#9c8979' />
+            </g>
+            {positiongrid.map((i) => defaultlayout(i))}
+            {positiongrid.map((numX) => updatedLayout(numX, positiongrid.indexOf(numX)))}
+          </svg>
+        </Grid2>
+      </Grid2>
+
+      <Grid2 container justifyContent={'center'} size={12}>
+        <Grid2 container justifyContent={'center'} className="footer" spacing={2}>
+          <Grid2 justifyContent={'center'}>
+            <Button variant="contained" className='footerButton' ><LuUndo2 size={28} /></Button>
+            <Typography ><IoRemoveOutline size={30} /><IoRemoveOutline size={30} /></Typography>
+          </Grid2>
+          <Grid2 justifyContent={'center'}>
+            <Button variant="contained" className='footerButton' ><RiSwap2Fill size={28} /></Button>
+            <Typography ><IoRemoveOutline size={30} /><IoRemoveOutline size={30} /></Typography>
+          </Grid2>
+          <Grid2 justifyContent={'center'}>
+            <Button variant="contained" className='footerButton' ><BsGridFill size={28} /></Button>
+            <Typography ><IoRemoveOutline size={30} /><IoRemoveOutline size={30} /></Typography>
+          </Grid2>
+          <Grid2 justifyContent={'center'} >
+            <Button variant="contained" className='footerButton' ><RiVipCrownFill size={28} /></Button>
+          </Grid2>
+        </Grid2>
+      </Grid2>
+
+      <Grid2 container justifyContent={'center'} size={12} marginTop={3}>
+        <Typography fontSize={14}>play2048.co © 2014—2024 Gabriele Cirulli. All rights reserved.</Typography>
+
+      </Grid2>
+
     </>
   );
 }
